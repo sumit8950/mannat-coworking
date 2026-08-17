f81f802d8a61c6ac0136fabe30ed48a8198410d3
sumit8950 <sumitk67966@gmail.com>
2026-08-17 12:07:31 +0530

    Add robots.txt for SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,4 @@
 import type { MetadataRoute } from "next";
-import { SITE_URL } from "@/app/seo";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -7,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: "https://www.mannatcoworking.com/sitemap.xml",
   };
 }
